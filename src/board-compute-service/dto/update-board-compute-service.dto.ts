9003d519640ff7b619f7af8bdae8c134529e1ac5
admin <admin@admins-MacBook-Pro.local>
2026-02-08 21:24:32 -0800

    fixed up da code

--- src/board-compute-service/dto/update-board-compute-service.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBoardComputeServiceDto } from './create-board-compute-service.dto';
-
-export class UpdateBoardComputeServiceDto extends PartialType(CreateBoardComputeServiceDto) {
-  id: number;
-}
